05b75195be4ca58cb9706873bea516332ef6c35d
bholenath <bholenath@DESKTOP-36LON94>
2017-07-01 15:32:08 +0530

    Add project files.

---
+++ MkCodeFirst/Controllers/EmployeeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MkCodeFirst.Models;
+using MkCodeFirst.UnitofWork;
+
+namespace MkCodeFirst.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private UnitofWorks uow = null;
+        //
+        // GET: /Emloyees/
+
+        public EmployeeController()
+        {
+            uow = new UnitofWorks();
+        }
+
+        public EmployeeController(UnitofWorks uow_)
+        {
+            this.uow = uow_;
+        }
+
+        public ActionResult Index()
+        {
+            return View(uow.Repository<Emloyee>().GetAll().ToList());
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Emloyees/Create
+
+        [HttpPost]
+        public ActionResult Create(Emloyee Emloyee)
+        {
+            if (ModelState.IsValid)
+            {
+                uow.Repository<Emloyee>().Insert(Emloyee);
+                uow.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(Emloyee);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
